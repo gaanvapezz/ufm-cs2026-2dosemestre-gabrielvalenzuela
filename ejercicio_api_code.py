c368feb6e6372ba6bbd3ecf74f2c8ff760cd42ec
gaanvapezz <gabrielvalenzuela@ufm.edu>
2026-09-18 20:24:50 -0600

    Incorporación de Obtener Datos y Opción de consultar
    Se incorpora contenido.

--- ejercicio_api_code.py
+++ ejercicio_api_code.py
@@ -3,10 +3,39 @@ import csv
 from io import StringIO
 
 def obtener_datos (simbolo):
-    return
+    URL = f"https://stooq.com/q/l/?s={simbolo}.us&f=sd2t2ohlcv&h&e=csv"
+    respuesta = requests.get(URL)
+    texto = respuesta.text
+    lineas = texto.slip("\n")
+
+    encabezados = lineas [0].split(",")
+
+    valores = lineas[1].split(",")
+    resultado = {}
+
+    for i in range (len(encabezados)):
+        resultado[encabezados[i]] = valores [i]
+
+        if resultado.get("Close") == "N/D": 
+            return None
+        return resultado
 
 def opcion_consultar ():
-    return
+    simbolo = input("Ingresa el símbolo búrsatil (ejemplos: aapl, tsla, ibm): ").lower()
+    datos = obtener_datos(simbolo)
+
+    if not datos: 
+        print("\n No se encontraron datos para ese símbolo - CONSULTA DE BOLSA DE VALORES")
+        return
+
+    print(f"\n--- Cotización de {datos['Symbol'].upper()} ---")
+    print(f"Fecha: {datos["Date"]}")
+    print(f"Apertura: ${datos["Open"]}")
+    print(f"Máximo: ${datos["High"]}")
+    print(f"Mínimo: ${datos["Low"]}")
+    print(f"Cierre: ${datos["Close"]}")
+    print(f"Volumen: {datos["Volume"]}")
+    
 
 def opcion_comparar ():
     return
